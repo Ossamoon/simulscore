a7b4cb1fc479c30c77c0cf7fe844393aefa044c3
Ossamoon <osamu950623@gmail.com>
2021-08-04 17:37:13 +0900

    create AuthProvider Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import "tailwindcss/tailwind.css";
 import { VFC } from "react";
 import type { AppProps } from "next/app";
 
+import { AuthProvider } from "components/auth";
+
 const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 };
 export default MyApp;
