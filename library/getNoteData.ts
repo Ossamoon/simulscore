7af468b8f1026925ef03e7a8cfdbaf6cd42d011e
Ossamoon <osamu950623@gmail.com>
2021-08-10 15:45:38 +0900

    create sample note data

---
+++ library/getNoteData.ts
@@ -0,0 +1,32 @@
+export type MemoData = { id: number; text: string };
+
+export type NoteData = {
+  title: string;
+  musicId: string;
+  memos: MemoData[];
+};
+
+const sampleData: NoteData = {
+  title: "初めてのメモ",
+  musicId: "A0011",
+  memos: [
+    { id: 41, text: "クラリネットとファゴットが主題を提示する" },
+    { id: 1008, text: "ホルンのソロが主題を提示する" },
+    { id: 3472, text: "最後のコーダ" },
+  ],
+};
+const sampleData2: NoteData = {
+  title: "2番目ののメモ",
+  musicId: "A0011",
+  memos: [
+    { id: 21, text: "第1楽章の練習記号A" },
+    { id: 1016, text: "第2楽章の練習記号A" },
+    { id: 2012, text: "第3楽章の練習記号A" },
+    { id: 3016, text: "第4楽章の練習記号A" },
+  ],
+};
+
+export const getNoteData = (musicId: string): NoteData[] => {
+  if (musicId === "A0011") return [sampleData, sampleData2];
+  return [];
+};
