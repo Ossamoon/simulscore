49a82b46534dcf8ba8e49ee6bd06a5d8558f23e1
Ossamoon <osamu950623@gmail.com>
2021-08-26 16:45:41 +0900

    enable to create new note

--- components/memoview.tsx
+++ components/memoview.tsx
@@ -5,12 +5,13 @@ import { AuthContext } from "components/auth";
 
 type MemoData = { id: number; text: string; color: "white" | "black" };
 
+type NoteTitle = { id: string; title: string };
+
 type NoteData = {
   id: string;
   title: string;
   musicId: string;
-  createdAt: firebase.firestore.Timestamp;
-  updatedAt: firebase.firestore.Timestamp;
+  timestamp: firebase.firestore.Timestamp;
   memos: MemoData[];
 };
 
@@ -19,8 +20,7 @@ const converter = {
     return {
       title: note.title,
       musicId: note.musicId,
-      createdAt: note.createdAt,
-      updatedAt: note.updatedAt,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       memos: note.memos,
     };
   },
@@ -33,8 +33,7 @@ const converter = {
       id: snapshot.id,
       title: data.title,
       musicId: data.musicId,
-      createdAt: data.createdAt,
-      updatedAt: data.updatedAt,
+      timestamp: data.timestamp,
       memos: data.memos,
     };
     return newNote;
@@ -59,6 +58,7 @@ export const MemoView: VFC<Props> = ({
 
   // Data & State
   const [data, setData] = useState<NoteData[] | null>(null);
+  const [newNoteTitle, setNewNoteTitle] = useState<NoteTitle | null>(null);
   const [displayingNoteId, setDisplayingNoteId] = useState<string | null>(null);
 
   useEffect(() => {
@@ -69,7 +69,7 @@ export const MemoView: VFC<Props> = ({
         .doc(currentUser?.uid)
         .collection("memos")
         .where("musicId", "==", musicId)
-        .orderBy("updatedAt", "desc")
+        .orderBy("timestamp", "desc")
         .withConverter(converter)
         .get()
         .then((querySnapshot) => {
@@ -89,7 +89,12 @@ export const MemoView: VFC<Props> = ({
   return (
     <>
       {/* メモ帳一覧 */}
-      <div className="w-min flex items-center hover:bg-warmGray-50 hover:shadow-md rounded-md cursor-pointer px-2 py-1 mb-1">
+      <div
+        onClick={() => {
+          setNewNoteTitle({ id: "", title: "" });
+        }}
+        className="w-min flex items-center hover:bg-warmGray-50 hover:shadow-md rounded-md cursor-pointer px-2 py-1 mb-1"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="18px"
@@ -104,46 +109,99 @@ export const MemoView: VFC<Props> = ({
           新しいメモ帳を作成
         </div>
       </div>
-      <div className="w-full h-20 space-y-2 bg-warmGray-200 rounded-md shadow-inner flex-nowrap overflow-y-auto pl-2 pr-4 py-2">
-        {data?.map((note) => {
-          return (
-            <div
-              key={note.id}
-              className={`w-full px-2 rounded-lg text-left truncate cursor-pointer hover:shadow-md ${
-                note.id === displayingNoteId
-                  ? "bg-blue-200 border border-blue-400"
-                  : "bg-white"
-              }`}
-              onClick={() => {
-                setDisplayingNoteId(
-                  note.id === displayingNoteId ? null : note.id
-                );
-              }}
-            >
-              <div
-                className={`text-base text-warmGray-600 pl-1 pt-1 pb-0.5  ${
-                  note.id === displayingNoteId ? "font-bold" : ""
-                }`}
-              >
-                {note.title}
-              </div>
-              <div
-                className={`text-xs italic border-t pl-1 py-px ${
-                  note.id === displayingNoteId
-                    ? "border-blue-300 text-warmGray-500"
-                    : "border-warmGray-300 text-warmGray-400"
-                }`}
-              >
-                <span>
-                  最終更新: {note.updatedAt.toDate().toLocaleDateString()}
-                </span>
-                <span className="ml-3">
-                  作成日: {note.createdAt.toDate().toLocaleDateString()}
-                </span>
+
+      <div className="relative w-full h-24">
+        {newNoteTitle === null ? null : (
+          <div className="absolute inset-0 w-full h-full rounded-md bg-black bg-opacity-20 p-3">
+            <div className="bg-white w-full rounded p-2">
+              <input
+                type="text"
+                value={newNoteTitle.title}
+                onChange={(e) => {
+                  setNewNoteTitle({
+                    id: "",
+                    title: e.target.value,
+                  });
+                }}
+                className="w-full border rounded px-1"
+              />
+              <div className="flex items-center pt-1">
+                <div
+                  onClick={() => {
+                    setNewNoteTitle(null);
+                  }}
+                  className="text-xs text-blue-600 px-2 cursor-pointer"
+                >
+                  キャンセル
+                </div>
+                <div
+                  onClick={() => {
+                    firebase
+                      .firestore()
+                      .collection("private_memos")
+                      .doc(currentUser?.uid)
+                      .collection("memos")
+                      .add({
+                        title: newNoteTitle.title,
+                        musicId: musicId,
+                        timestamp:
+                          firebase.firestore.FieldValue.serverTimestamp(),
+                        memos: [],
+                      })
+                      .then((docRef) => {
+                        console.log("Document written with ID: ", docRef.id);
+                      })
+                      .catch((error) => {
+                        console.error("Error adding document: ", error);
+                      });
+                    setNewNoteTitle(null);
+                  }}
+                  className="text-xs bg-red-500 rounded text-white cursor-pointer px-2 py-1"
+                >
+                  作成
+                </div>
               </div>
             </div>
-          );
-        })}
+          </div>
+        )}
+        <div className="w-full h-full space-y-2 bg-warmGray-200 rounded-md shadow-inner flex-nowrap overflow-y-auto pl-2 pr-4 py-2">
+          {data?.map((note) => {
+            return (
+              <div
+                key={note.id}
+                className={`w-full px-2 rounded-lg text-left truncate cursor-pointer hover:shadow-md ${
+                  note.id === displayingNoteId
+                    ? "bg-blue-200 border border-blue-400"
+                    : "bg-white"
+                }`}
+                onClick={() => {
+                  setDisplayingNoteId(
+                    note.id === displayingNoteId ? null : note.id
+                  );
+                }}
+              >
+                <div
+                  className={`text-base text-warmGray-600 pl-1 pt-1 pb-0.5  ${
+                    note.id === displayingNoteId ? "font-bold" : ""
+                  }`}
+                >
+                  {note.title}
+                </div>
+                <div
+                  className={`text-xs italic border-t pl-1 py-px ${
+                    note.id === displayingNoteId
+                      ? "border-blue-300 text-warmGray-500"
+                      : "border-warmGray-300 text-warmGray-400"
+                  }`}
+                >
+                  <span>
+                    最終保存: {note.timestamp.toDate().toLocaleString()}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
 
       {/* メモ内容 */}
@@ -161,7 +219,7 @@ export const MemoView: VFC<Props> = ({
               <path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
             </svg>
             <div className="w-max text-sm text-warmGray-600 pl-1">
-              新しいメモを追加
+              メモの追加
             </div>
           </div>
           <div className="w-full h-96 space-y-3 bg-warmGray-200 rounded-md shadow-inner flex-nowrap overflow-y-auto pl-2 pr-4 py-2">
